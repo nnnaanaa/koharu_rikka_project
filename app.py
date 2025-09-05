890d5dc0f3fd1d117caa2977af9f5108f24b3001
nnnaanaa <ryuu_yoshi_22_0417@yahoo.co.jp>
2025-09-05 22:03:33 +0900

    init

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sounddevice as sd
 import soundfile as sf
 
-filename = "./soundfile/Music/hello_v2.wav"  # 修正: Windows の `\` ではなく Linux の `/` を使う
+filename = "/python/koharu_rikka_project/soundfile/Music/hello_v2.wav"  # 修正: Windows の `\` ではなく Linux の `/` を使う
 data, samplerate = sf.read(filename)
 sd.play(data, samplerate)
 sd.wait()
